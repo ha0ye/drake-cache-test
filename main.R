adc54ae7ca204e377a0f0540e202bfa83408f379
Hao Ye <lhopitalified@gmail.com>
2019-08-13 15:27:05 -0400

    try `file_in` for dataset trigger

--- main.R
+++ main.R
@@ -6,8 +6,11 @@ expose_imports(MATSS)
 
 pipeline <- drake_plan(
     lizards = get_cowley_lizards(), 
-    snakes = get_cowley_snakes()
+    snakes = get_cowley_snakes(), 
+    mtquad = get_mtquad_data(file_in(!!file.path(get_default_data_path(), "mapped-plant-quads-mt"))), 
 )
+
+pipeline$trigger <- rlang::expr("trigger(command = FALSE)")
 
 db <- DBI::dbConnect(RSQLite::SQLite(), here::here("drake-cache.sqlite"))
 cache <- storr::storr_dbi("datatable", "keystable", db)
@@ -18,5 +21,5 @@ vis_drake_graph(config, build_times = "none")
 make(pipeline, 
      cache = cache, 
      parallelism = "future", 
-     cache_log_file = TRUE, 
+     cache_log_file = here::here("drake-cache-log.csv"), 
      jobs = 2)
